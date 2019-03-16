9cffd12d9693ee4424f4120c77e144842e42851a
fujiokayu <fujiokayu@gmail.com>
2019-03-17 00:39:50 +0900

    Error line 16: i and j are Different declared type

--- gosample.go
+++ gosample.go
@@ -4,10 +4,14 @@ import (
 	"errors"
 )
 
-func Div(i, j int) (int, error) {
+type firstVal int
+type secondVal int
+
+func Div(i firstVal, j secondVal) (int, error) {
 	if j == 0 {
 		// return my error
 		return 0, errors.New("divied by zero")
 	}
+
 	return i / j, nil
 }
