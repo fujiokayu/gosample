07a2281f2bdf015e3983dc71141a8af12e0ee553
fujiokayu <fujiokayu@gmail.com>
2019-03-16 12:04:19 +0900

    implement error
    http://gihyo.jp/dev/feature/01/go_4beginners/0002?page=3

--- gosample.go
+++ gosample.go
@@ -1,4 +1,13 @@
 package gosample
 
-var Message string = "hello world"
+import (
+	"errors"
+)
 
+func div(i, j int) (int, error) {
+	if j == 0 {
+		// return my error
+		return 0, errors.New("divied by zero")
+	}
+	return i / j, nil
+}
