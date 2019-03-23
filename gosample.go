35b6428bfc2199ce8eb3a3bd05cac6386a035b10
fujiokayu <fujiokayu@gmail.com>
2019-03-23 17:29:04 +0900

    Add reading file sample

--- gosample.go
+++ gosample.go
@@ -2,6 +2,7 @@ package gosample
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
@@ -39,4 +40,21 @@ func Write() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Read() {
+	file, err := os.Open("./file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	message := make([]byte, 12)
+	_, err = file.Read(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(message))
+	fmt.Print(string("\n"))
 }
